0df9cc7d77d79c5a22c18fc6bcf66d9488020f9a
G-who0212 <gwho990212@gmai.com>
2024-12-19 20:32:27 +0900

    jenkins added

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Clone repository') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Build image') {
+            steps {
+                script {
+                    app = docker.build("gwho0212/countfit-ai:${env.BUILD_NUMBER}")
+                }
+            }
+        }
+
+        stage('Push image') {
+            steps {
+                script {
+                    docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+                        app.push("${env.BUILD_NUMBER}")
+                        app.push("latest")
+                    }
+                }
+            }
+        }
+
+        stage('Deploy to EC2') {
+            steps {
+                sshagent(['countfit-backend-ec2-ssh-key']) {
+                    withCredentials([
+                        string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID'),
+                        string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')
+                    ]) {
+                        sh '''
+ssh -o StrictHostKeyChecking=no ubuntu@52.78.27.46 <<EOF
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+
+docker pull gwho0212/countfit-ai:latest
+docker stop countfit-ai || true
+docker rm countfit-ai || true
+docker run -d --name countfit-ai -p 8000:8000 gwho0212/countfit-ai:latest
+EOF
+                        '''
+                    }
+                }
+            }
+        }
+    }
+}
